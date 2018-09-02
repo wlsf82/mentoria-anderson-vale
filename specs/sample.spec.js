33efa6a5c32a09aa68b492ca16791e974283a59f
Walmyr <wlsf82@gmail.com>
2018-09-02 19:42:39 +0200

    Add test scenarios skeleton

--- specs/sample.spec.js
+++ specs/sample.spec.js
@@ -22,4 +22,24 @@ describe('Hackernews fake', () => {
 
         expect(loading.isDisplayed()).toBe(true);
       });
+
+      it('renders 200 items after clicking the More button', () => {
+        // Write test logic here.
+      });
+
+      it('quickly shows a loading component when searching for the word "react" for the first time', () => {
+        // Write test logic here.
+      });
+
+      it('renders 100 items after searching for the word "react" for the first time', () => {
+        // Write test logic here.
+      });
+
+      it('does not renders a loading component after searching for "react" and then "redux" again', () => {
+        // Write test logic here.
+      });
+
+      it('shows only 99 items after dismissing one item', () => {
+        // Write test logic here.
+      });
 });
